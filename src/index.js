a20015edb0f7adfd22116d7feb24eb16a9c5698e
Kubilay Özdemir <kubilayozdemir95@gmail.com>
2023-08-02 21:22:35 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { createLogger } from 'redux-logger';
 import { Provider } from "react-redux";
 import reducer from "./reducer";
 
-const store = createStore(reducer);
+const logger = createLogger();
+const middleware = applyMiddleware(logger);
+const store = createStore(reducer,middleware);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
